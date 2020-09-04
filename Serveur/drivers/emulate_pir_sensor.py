cf56da03c61b2eff0dfda1e2041a4959c99295e6
anasmansouri <htmlphpcss3@gmail.com>
2020-09-04 14:04:49 +0100

    emulation of the pir sensor

---
+++ Serveur/drivers/emulate_pir_sensor.py
@@ -0,0 +1,6 @@
+#!/user/bin/python3
+import sys
+from ..mybiblio.mybiblio import emulate_detection_of_a_person
+
+if __name__ == "__main__":
+    emulate_detection_of_a_person()
---
+++ Serveur/mybiblio/mybiblio.py
@@ -0,0 +1,23 @@
+import time
+from random import randint
+
+
+def emulate_detection_of_a_person():
+    path = "/home/anas/secret_room_security_system/Serveur/dev/pir"
+    put_the_output_high(path)
+    time.sleep(5)
+    put_the_output_low(path)
+
+
+def put_the_output_high(path_to_file):
+    write_value_in_file(path_to_file, 1)
+
+
+def put_the_output_low(path_to_file):
+    write_value_in_file(path_to_file, 0)
+
+
+def write_value_in_file(path_to_file, value):
+    f = open(path_to_file, "w")
+    f.write("{}".format(value))
+    f.close()
