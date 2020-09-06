aec689fa39832e618ad1c58a40c4978fadf35449
anasmansouri <htmlphpcss3@gmail.com>
2020-09-06 13:07:08 +0100

    start testing

--- Serveur/drivers/pwm/test.c
+++ Serveur/drivers/pwm/test.c
@@ -5,6 +5,7 @@
 int main(){
 pwm_channel pwm;
 load_pwm_channel(&pwm,"0");
-set_pwm_period(&pwm, 100);
+set_pwm_period(&pwm, 500000);
+set_pwm_duty_cycle(&pwm, 70000);
 // set_pwm_polarity(&pwm, "inversed");
 }
