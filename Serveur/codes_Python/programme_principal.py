744e89795b52a4917a63f037b504fdcafaeec6b5
anasmansouri <htmlphpcss3@gmail.com>
2020-09-07 19:40:06 +0100

    the path problem is done

--- Serveur/codes_Python/programme_principal.py
+++ Serveur/codes_Python/programme_principal.py
@@ -1,11 +1,25 @@
 import subprocess
+import os
+import sys
+
+sys.path.append("../mybiblio")
+from mybiblio import write_value_in_file
+
+# write_value_in_file("./file", "adf")
 
 
 def function():
-    out = subprocess.Popen(['sudo', './', '../drivers/mouvement_detection_sensor/read_pir_value'],
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.STDOUT)
-    stdout, stderr = out.communicate()
+    f = os.popen("../drivers/mouvement_detection_sensor/read_pir_value")
+    sensor_value = f.read()
+    if sensor_value == "1":
+        print("person detected")
+        # write 1 in file_pip
+        # waite for 30 seconds
+        # test if the content of the file is still 1
+        # if yes  then lunch the buzzer
+        # else do nothing
 
 
-function()
+if __name__ == "__main__":
+
+    function()
