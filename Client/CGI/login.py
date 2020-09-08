82ef8e6dc87adf89946fee94b8f88c7767994b6a
anasmansouri <htmlphpcss3@gmail.com>
2020-09-08 13:48:11 +0100

    cgit python fixing

--- Client/CGI/login.py
+++ Client/CGI/login.py
@@ -6,26 +6,24 @@ print("<body>")
 
 
 def take_the_client_info(form):
-    print(""""
-    <h1>Type your info</h1>
 
-        <form method="post">
-          <label for="userId">User id:  </label>
-          <input type="text" id="userId" name="userId"><br><br>
-          <label for="pwd">Password:</label>
-          <input type="password" id="pwd" name="pwd"> <br><br>
-          <input type="submit" value="Submit">
-        </form>
-    
-    """)
-    if "submit" in form and "userId" in form and "pwd" in form:
+    print("<h1>Type your info</h1>")
+    print("<form method=\"post\">")
+    print("<label for=\"userId\">User id:  </label>")
+    print("<input type=\"text\" id=\"userId\" name=\"userId\"><br><br>")
+    print("<label for=\"pwd\">Password:</label>")
+    print("<input type=\"password\" id=\"pwd\" name=\"pwd\"> <br><br>")
+    print("<input type=\"submit\" value=\"Submit\">")
+    print("</form>")
+
+    if "submit" in form :
         if form["userId"] == "anas" and form["pwd"] == "123456789":
             print("<p> stop buzzer <p>")
             print("go to the welcome page")
         else:
             print("<p> the password is wrong please try again <p>")
 
-
+cgi.enable()
 form = cgi.FieldStorage()
 take_the_client_info(form)
 
