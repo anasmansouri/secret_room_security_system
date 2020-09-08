7987a2353532453a51c3d5807e4389eb7c291a4a
anasmansouri <htmlphpcss3@gmail.com>
2020-09-08 01:25:01 +0100

    the pwm is tested

--- Serveur/codes_C/launch_buzzer.c
+++ Serveur/codes_C/launch_buzzer.c
@@ -1,12 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
 #include "../drivers/pwm/pwm.h"
 
-int main(){
+int main(int argc, char **argv){
 
+
+if(argc !=3){
+printf("please enter the period and the duty cycle ");
+exit(1);
+}
 
 pwm_channel pwm;
 load_pwm_channel(&pwm,"0");
-set_pwm_period(&pwm, 160000000);
-set_pwm_duty_cycle(&pwm, 70000000);
+
+char * period = argv[1];
+char * duty_cycle = argv[2];
+set_pwm_period(&pwm, period);
+set_pwm_duty_cycle(&pwm, duty_cycle);
 set_pwm_polarity(&pwm, "normal");
 enable_pwm_channel(&pwm);
 
