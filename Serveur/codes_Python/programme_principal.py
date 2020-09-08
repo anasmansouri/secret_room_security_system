b94a6b2bb0b7bd74dc7c58a40b043168587f8a5b
anasmansouri <htmlphpcss3@gmail.com>
2020-09-08 01:33:14 +0100

    launch buzzer after 30 seconds of the detection of the person

--- Serveur/codes_Python/programme_principal.py
+++ Serveur/codes_Python/programme_principal.py
@@ -17,7 +17,7 @@ def function():
         write_value_in_file("./file", "1")
         time.sleep(30)
         if read_the_content_of_file("./file") == "1":
-            print("launch buzzer")
+            os.popen("../codes_C/launch_buzzer 9000 7000")
         else:
             print("your welcome in the secret room")
 
