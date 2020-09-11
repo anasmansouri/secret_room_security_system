0596026c3d193995e38f95a8a520e4124b59aa3a
anasmansouri <htmlphpcss3@gmail.com>
2020-09-11 13:50:52 +0100

    show history

---
+++ Client/Php/show_history.php
@@ -0,0 +1,13 @@
+<?php
+//Connexion a la base de donnees
+$dbh = new PDO("sqlite:/home/pi/project_mansouri/secret_room_security_system/Client/BD/room_management.db");
+//Requetes de selection
+$sql = "SELECT * FROM history";
+// affichage des resultats
+echo "<h>history : </h>"
+foreach ($dbh->query($sql) as $row)
+{
+    echo $row[time]." | ".$row[user_id]."<br>";
+}
+$dbh = null;
+?>
