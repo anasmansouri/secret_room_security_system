825c9658181c5ec942aab6bd2afadf1ed2a1df31
anasmansouri <htmlphpcss3@gmail.com>
2020-09-11 12:20:17 +0100

    add history system

--- Client/CGI/test.py
+++ Client/CGI/test.py
@@ -5,12 +5,17 @@ import sys
 import subprocess
 import sqlite3
 
+sys.path.append("/home/pi/project_mansouri/secret_room_security_system/Serveur/mybiblio")
+from mybiblio import write_value_in_file
+
 
 def check_the_user_info(user_id, password):
     connection = sqlite3.connect("/www/room_management.db")
     cursor = connection.cursor()
     for row in cursor.execute("SELECT * FROM USERS"):
         if (str(row[0]) == user_id) and (str(row[1]) == password):
+            cursor.execute("INSERT INTO HISTORY VALUES('{}',datetime('now'))".format(row[0]))
+            connection.commit()
             return True
     return False
 
@@ -24,8 +29,8 @@ def take_user_info():
 
     if "login" in form:
         if check_the_user_info(form["user_id"].value, form["password"].value):
-            print("<h2>user name is  {} est : </h2>".format(form["user_id"].value))
-        else:
+            write_value_in_file("/home/pi/project_mansouri/secret_room_security_system/Serveur/codes_Python/file", "0")
+         else:
             print("<p> the user name or the password is wrong </p>")
 
 
