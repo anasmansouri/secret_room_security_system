e2ead07608a213e38be02daa0517ead76485015e
anasmansouri <htmlphpcss3@gmail.com>
2020-09-16 04:06:36 +0100

    final commit

--- Client/CGI/login.py
+++ Client/CGI/login.py
@@ -4,6 +4,8 @@ import cgitb
 import sys
 import subprocess
 import sqlite3
+import os
+
 
 sys.path.append("/home/pi/project_mansouri/secret_room_security_system/Serveur/mybiblio")
 from mybiblio import write_value_in_file
@@ -14,6 +16,7 @@ def check_the_user_info(user_id, password):
     cursor = connection.cursor()
     for row in cursor.execute("SELECT * FROM USERS"):
         if (str(row[0]) == user_id) and (str(row[1]) == password):
+            
             cursor.execute("INSERT INTO HISTORY VALUES(datetime('now'),'{}')".format(row[0]))
             connection.commit()
             return True
@@ -23,6 +26,7 @@ def check_the_user_info(user_id, password):
 def take_user_info():
     if ("login" in form) and (check_the_user_info(form["user_id"].value, form["password"].value)):
         arreter_le_declenchement_du_buzzer()
+        stop_buzzer()
         print("<h> your welcome to the secret room</h>")
     else:
         print('<h1>type your info:</h1>')
@@ -30,10 +34,12 @@ def take_user_info():
         print('<br> <input type = "text" name = "user_id" placeholder ="user_id"/>')
         print('<br> <input type = "password" name = "password" placeholder ="password"/>')
         print(' <input type = "submit" name = "login" value = "login"/>')
+def stop_buzzer():
+    os.popen("sudo /home/pi/project_mansouri/secret_room_security_system/Serveur/codes_C/launch_buzzer 0 0")
+
 
 def arreter_le_declenchement_du_buzzer():
     write_value_in_file("/home/pi/project_mansouri/secret_room_security_system/Serveur/tunnel/controle_buzzer", "0")
-       
 
 print("Content-type: text/html\n\n")
 print("<body>")
@@ -43,3 +49,4 @@ take_user_info()
 print("</form>")
 print("</body>")
 print("</html>")
+
--- Serveur/codes_Python/programme_principal.py
+++ Serveur/codes_Python/programme_principal.py
@@ -9,24 +9,26 @@ from mybiblio import write_value_in_file, read_the_content_of_file
 
 
 def function():
-    print("hadi f5ater")
     f = os.popen("sudo ../drivers/mouvement_detection_sensor/read_pir_value")
     sensor_value = f.read()
-    print(sensor_value)
     if sensor_value == "1":
         print("person detected")
         write_value_in_file("/home/pi/project_mansouri/secret_room_security_system/Serveur/tunnel/controle_buzzer", "1")
         write_value_in_file("../drivers/mouvement_detection_sensor/pir", "0")
-        time.sleep(10)
+        time.sleep(20)
         if read_the_content_of_file("/home/pi/project_mansouri/secret_room_security_system/Serveur/tunnel/controle_buzzer") == "1":
-            os.popen("sudo ../codes_C/launch_buzzer 160000000 70000000")
+            os.popen("sudo ../codes_C/launch_buzzer 900000 70000")
             print("launch buzzer")
         else:
             print("your welcome in the secret room")
 
 
 if __name__ == "__main__":
-    while True :
-        time.sleep(5)
-        function()
+    try:
+        while True :
+            time.sleep(1)
+            function()
+    except KeyboardInterrupt:
+        print ('programme stoped')
+        sys.exit(0)
 
